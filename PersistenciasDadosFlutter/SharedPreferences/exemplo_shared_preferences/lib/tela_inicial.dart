2ef79a4d42f14d21a4e8c20346f87d2a14a38a9b
Manuela2170 <manuelaviolimlacerda@gmail.com>
2025-10-15 11:16:36 -0300

    15.10.25

--- PersistenciasDadosFlutter/SharedPreferences/exemplo_shared_preferences/lib/tela_inicial.dart
+++ PersistenciasDadosFlutter/SharedPreferences/exemplo_shared_preferences/lib/tela_inicial.dart
@@ -1,114 +1,95 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart'; //Importa a biblioteca SharedPreferences
+import 'package:shared_preferences/shared_preferences.dart';
 
-class TelaInicial extends StatefulWidget {
-  //Tela Dinâmica (mudança de estado)
+class TelaInicial extends StatefulWidget{
+  const TelaInicial({super.key});
+ //tela com mudança de state
   @override
-  _TelaInicialState createState() => _TelaInicialState(); //Chama da Mudança de Estado
+  _TelaInicialState createState() => _TelaInicialState(); //chama a mudança
 }
 
-class _TelaInicialState extends State<TelaInicial> {
-  //Estado da Tela Inicial
+class _TelaInicialState extends State<TelaInicial>{ //ação/construção do tela
   //atributos
-  TextEditingController _nomeController = TextEditingController(); //Recebe informações TextField
-  String _nome = ""; // Atributo que Armazena o Nome do Usuário
-  bool _darkMode = false; //Atributo que armazena o modo escuro
+  final TextEditingController _nomeController = TextEditingController(); //variavel que recebe info do textField
+  String _nome = "";
+  bool _darkMode = false;
 
-  // método InitState ->
+  //métodos
+
   @override
-  void initState() {
-    //método para iniciar a tela
-    super.initState();
-    _carregarPreferencias();
+  void initState() { //roda antes de iniciar a tela
+    // TODO: implement initState
+   super.initState();
+   _carregarPreferencias();
   }
 
-  //método para Carregar Nome do Usuário
-  void _carregarPreferencias() async {
-    //método assincrono
-    SharedPreferences _prefs = await SharedPreferences.getInstance(); //pegar as informações do cache
-    _nome = _prefs.getString("nome") ?? ""; //pega o nome do usuário no shared_
-    _darkMode = _prefs.getBool("darkMode") ?? false; //pega o modo escuro shared_
+  void _carregarPreferencias() async{  //roda de forma assincrona ( junto com outra função/método)
+    //estabelecer conexão com a memoria(cache)
+    SharedPreferences prefs = await SharedPreferences.getInstance(); //instalar a dependencia/biblioteca
+    _nome = prefs.getString("nome") ?? ""; //buscar o nome do usuario
+    _darkMode = prefs.getBool("darkMode") ?? false; //buscar o modo de tela do usuario
     setState(() {
-      // recarregar a tela
+      
     });
   }
 
-  //Método para Salvar o Nome do Usuário
-  void _salvarNome() async {
-    //adicinar o salar no shared preferences
-    SharedPreferences _prefs = await SharedPreferences.getInstance();
+  void _salvarNome() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     _nome = _nomeController.text.trim();
-    if (_nome.isEmpty) {
-      //Madar uma Mensagem para o usuário
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text("Informe um Nome Válido!")));
-    } else {
-      _nomeController.clear(); //limpa o TextField
-      _prefs.setString("nome", _nome); //salva o nome no sharedPref
+    if(_nome.isEmpty){
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Informe um Nome Válido!!!")));
+    }else{
+      prefs.setString("nome", _nome);
       setState(() {
-        //Atualizar o Nome do usuário na Tela
-        ScaffoldMessenger.of(
-          context,
-        ).showSnackBar(SnackBar(content: Text("Nome do Usuário Atualizado!")));
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Nome do Usuário Atualizado!!!")));
+        _nomeController.clear();
       });
     }
   }
 
-  //método salvar Modo Escuro
   void _salvarModoEscuro() async{
-    SharedPreferences _prefs = await SharedPreferences.getInstance();
-    _darkMode = !_darkMode; //inverte o valor do darkmode(atributo)
-    _prefs.setBool("darkMode", _darkMode); //salvo no Shared
+    SharedPreferences prefs = await SharedPreferences.getInstance();//estabelecer conexão om a memória interna (shared Preferences)
+    _darkMode = !_darkMode;
+    prefs.setBool("darkMode", _darkMode);
     setState(() {
-      //atualiza a tela
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Modo Escuro ${_darkMode ? "Ativado" : "Desativado"}")));
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Modo Escuro ${_darkMode ? "Ativado" : "Desativado"}")));
     });
+
   }
 
-  
-
   @override
-  Widget build(BuildContext context) {
-    // Constroi a Tela
+  Widget build(BuildContext context){ //construtor de estrutura
     return AnimatedTheme(
-      //muda o tema da tela
-      data: _darkmode ? ThemeData.dark() : ThemeData.light(),
+      data: _darkMode ? ThemeData.dark() : ThemeData.light(),
       child: Scaffold(
-        //estrutura básica da tela
         appBar: AppBar(
-          title: Text("Bem-vindo ${_nome == "" ? "Visitante" : _nome}"),
+          title: Text("Bem-Vindo ${_nome=="" ? "Visitante" : _nome}"),
           actions: [
-            IconButton(onPressed:_salvarModoEscuro, 
+            IconButton(
+              onPressed: _salvarModoEscuro, 
               icon: Icon(_darkMode ? Icons.light_mode : Icons.dark_mode))
-          ],
-        ),
+          ],),
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               TextField(
                 controller: _nomeController,
-                decoration: InputDecoration(labelText: "Informe seu Nome"),
+                decoration: InputDecoration(labelText: "Informe Seu Nome"),
               ),
               ElevatedButton(
-                onPressed: _salvarNome,
-                child: Text("Salvar Nome do Usuário"),
-              ),
+                onPressed: _salvarNome, 
+                child: Text("Salvar Nome do Usuário")),
+              SizedBox(height: 20,),
+              ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context, "/tarefas"), //navegar para a tela de tarefas
+                child: Text("Tarefas do $_nome"),)
             ],
           ),
         ),
       ),
     );
   }
-}
 
-// o que é o SharedPreferences?
-// é uma biblioteca de armazenamento de dados interna do aplicativo (cache do app)
-//como ela funciona?
-// armazena dados na condição de chave-valor(key-value)
-// nome -> _nome
-// tipos de dados armazenados no Shared preferences:
-// String, int, double, bool, List<String>
-//métodos dos shared preferences:
-// getters and setters
+}
